29858c723609a676c12c7864e5f49368265e481a
Julian Lindner <jl@xq0.net>
2022-10-07 16:54:28 +0200

    changed some colours and favicon

--- src/components/SectionHero/SectionHero.tsx
+++ src/components/SectionHero/SectionHero.tsx
@@ -16,7 +16,7 @@ const SectionHero: FC<SectionHeroProps> = ({ className = "" }) => {
       <div className="flex flex-col lg:flex-row lg:items-center">
         <div className="flex-shrink-0 lg:w-1/2 flex flex-col items-start space-y-8 sm:space-y-10 pb-14 lg:pb-64 xl:pr-14 lg:mr-10 xl:mr-0">
           <h2 className="font-medium text-4xl md:text-5xl xl:text-7xl !leading-[114%] ">
-            Join the <b>Kampong Social Club</b>today!
+            Join the <strong>Kampong Social Club</strong> today!
           </h2>
           <span className="text-base md:text-lg text-neutral-500 dark:text-neutral-400">
             You're done with the average? You want to meet a great community? You want to connect with awesome people? You're an entrepreneur, a digital nomad, an astronaut – or just in general an interesting person?
